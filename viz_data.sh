50b00d60cb98222702708e229f87b10c5311b808
levan92 <lingevan0208@gmail.com>
2021-06-24 02:49:44 +0800

    clean up rm shs

--- viz_data.sh
+++
@@ -1,1 +0,0 @@
-python3 viz_data.py --config-file configs/ftrcnn-r50-IN-LSJ.yaml --datasets-dir /media/dh/HDD/pp/Labrador200/ --datasets-train Labrador200_merged_train --datasets-test Labrador200_merged_val 
