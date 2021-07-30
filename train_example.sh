f13df1ff0390ba141b70e4a2cc71b771f4fef9eb
levan92 <lingevan0208@gmail.com>
2021-07-30 15:34:43 +0800

    train example script update

--- train_example.sh
+++ train_example.sh
@@ -9,6 +9,7 @@
 # --download-models \
 # --s3-models-bucket \
 # --s3-models-path \
+# --model-weights \
 # --download-data \
 # --s3-data-bucket \
 # --s3-data-path \
@@ -17,16 +18,31 @@
 # --s3-output-bucket \
 # --s3-output-path
 
-# training locally completely
+# # training locally completely
+# python3 train_net_clearml.py \
+# --config-file configs/det2_clearml/example.yaml \
+# --skip-clearml \
+# --skip-s3 \
+# --custom-dsnames coco_train coco_val \
+# --custom-cocojsons /media/dh/HDD/coco/annotations/instances_train2017.json /media/dh/HDD/coco/smallval/instances_val2017.json \
+# --custom-imgroots /media/dh/HDD/coco/train2017 /media/dh/HDD/coco/smallval/images \
+# --datasets-train coco_train \
+# --datasets-test coco_val 
+
+# training locally but with clearml expt training
 python3 train_net_clearml.py \
---config-file configs/det2_clearml/example.yaml \
---skip-clearml \
+--clearml-run-locally \
+--clearml-proj det2 \
+--clearml-task-name coco_run \
+--clearml-task-type training \
 --skip-s3 \
---custom-dsnames coco_train coco_val \
---custom-cocojsons /media/dh/HDD/coco/annotations/instances_train2017.json /media/dh/HDD/coco/smallval/instances_val2017.json \
---custom-imgroots /media/dh/HDD/coco/train2017 /media/dh/HDD/coco/smallval/images \
+--config-file configs/ftrcnn-r50-IN-LSJ.yaml \
+--model-weights /media/dh/HDD/workspace/detectron2/weights/faster_rcnn_R_50_FPN_3x/model_final_280758.pkl \
 --datasets-train coco_train \
---datasets-test coco_val 
+--datasets-test coco_val \
+--custom-dsnames coco_train coco_val \
+--custom-cocojsons /media/dh/HDD/coco/annotations/instances_train2017.json /media/dh/HDD/coco/annotations/instances_val2017.json \
+--custom-imgroots /media/dh/HDD/coco/train2017 /media/dh/HDD/coco/val2017
 
 # # training with clearml & s3 with multi-gpus
 # python3 train_net_clearml.py \
@@ -42,6 +58,7 @@ python3 train_net_clearml.py \
 # --download-models \
 # --s3-models-bucket \
 # --s3-models-path \
+# --model-weights \
 # --download-data \
 # --s3-data-bucket \
 # --s3-data-path \
