09badc218dc0b7af6949174c8c8acccca8e32443
levan92 <lingevan0208@gmail.com>
2021-11-05 15:28:43 +0800

    doc for s3 direct reading

--- data/data_mapper.py
+++ data/data_mapper.py
@@ -92,8 +92,8 @@ def read_image_s3(path, s3_info, format=None):
     Will apply rotation and flipping if the image has such exif information.
 
     Args:
-        bucket (str)
         path (str): image file path from bucket
+        s3_info (dict): dictionary of information needed for s3 communication (endpoint_url, bucket, aws_access_key_id, aws_secret_access_key, region_name, verify)
         format (str): one of the supported image modes in PIL, or "BGR" or "YUV-BT.601".
 
     Returns:
@@ -180,12 +180,14 @@ class AugDatasetMapper:
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
 
         if self.s3_info:
+            # download from s3 into memory and directly read it  
             image = read_image_s3(
                 dataset_dict["file_name"],
                 self.s3_info,
                 format=self.img_format,
             )
         else:
+            # detectron2 default image reading (from local filesystem)
             image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
         utils.check_image_size(dataset_dict, image)
 
